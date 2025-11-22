85264248ce4c77f4f74e6295ce5d4bc9b52898c9
viviaan09 <ngvivian682@gmail.com>
2025-11-22 18:41:49 +0700

    update addmovie.tsx

--- src/pages/movies/AddMovie.tsx
+++ src/pages/movies/AddMovie.tsx
@@ -1,9 +1,84 @@
+import { useState, type ChangeEvent, type FormEvent } from "react"
+import { Button, Form } from "react-bootstrap"
 import { NavLink } from "react-router"
+import ApiClient from "../../utils/ApiClient"
+
+interface FormMovie{
+    judul: string,
+    tahunRilis : string,
+    sutradara : string
+}
 
 function AddMovie(){
-    return <div>
-        <h2>Add Movie Page</h2>
-        <NavLink to ="/" className="btn btn-primary">List Movies</NavLink>
+    const [form, setForm]=useState({
+        judul: "",
+        tahunRilis : "",
+        sutradara : ""
+    })
+
+const handleInputChange = (event : ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = event.target
+
+    setForm({
+        ...form,
+        [name] : value 
+    })
+}
+
+const handleSubmit = async (event : FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try{
+        const response = await ApiClient.post('/movie', form)
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+    return <div className="container mx-auto">
+        <div className="d-flex justify-content-between mb-3">
+            <h4>Add Movie Page</h4>
+            <NavLink to ="/" className="btn btn-primary">List Movies</NavLink>
+        </div>
+
+        <div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formJudul">
+                    <Form.Label>Judul</Form.Label>
+                    <Form.Control 
+                        value = {form.judul}
+                        onChange={handleInputChange}
+                        name="judul" 
+                        type="text" 
+                        placeholder="Judul Film" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formTahunRilis">
+                    <Form.Label>Tahun Rilis</Form.Label>
+                    <Form.Control 
+                        value = {form.tahunRilis}
+                        onChange={handleInputChange}
+                        name="tahunRilis" 
+                        type="text" 
+                        placeholder="Tahun Rilis" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formSutradara">
+                    <Form.Label>Sutradara</Form.Label>
+                    <Form.Control 
+                        value = {form.sutradara}
+                        onChange={handleInputChange}
+                        name="sutradara" 
+                        type="text" 
+                        placeholder="Sutradara" />
+                </Form.Group>
+
+                <Button type="submit" variant="primary">
+                    Simpan
+                </Button>
+
+            </Form>
+        </div>
     </div>
 }
 
--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -1,33 +1,79 @@
 import { useCallback, useEffect, useState } from "react";
 import { NavLink } from "react-router";
 import ApiClient from "../../utils/ApiClient";
+import Table from 'react-bootstrap/Table';
+import { Button } from "react-bootstrap";
 
-interface Movie{
-    id : string,
+interface Movie {
+    _id : string,
     judul : string,
     tahunRilis : string,
     sutradara : string,
     createdAt : string,
-    updateAt : string 
+    updatedAt : string
 }
+
 function Movies(){
     const [movies, setMovies] = useState<Movie[]>([])
+    const [loading, setLoading] = useState<boolean>(true)
 
     const fetchMovies = useCallback(async () => {
+        setLoading(true)
         const response = await ApiClient.get("/movie")
-
-        if (response.status ==200){
+        
+        if(response.status == 200){
             setMovies(response.data.data)
+            setLoading(false)
         }
-    },[])
+    }, [])
 
     useEffect(() => {
         fetchMovies()
-    },[fetchMovies])
-    return <div className="container mx-auto">
-        <div className="d-flex justify-content-between mb-3">
-        <h4>Movie Page</h4>
-        <NavLink to ="/add-movie"className="btn btn-primary">Add Movie</NavLink>
+    }, [fetchMovies])
+
+    const handleDelete = async(movieId : String) => {
+        const response = await ApiClient.delete(`/movie/${movieId}`)
+
+        if (response.status == 200){
+            fetchMovies()
+        }
+    }
+
+    return <div className = "container mx-auto">
+        <div className = "d-flex justify-content-between mb-3">
+            <h4>Movie Page</h4>
+            <NavLink to ="/add-movie" className="btn btn-primary">Add Movie</NavLink>
+        </div>
+        <div>
+            <Table striped bordered hover>
+                <thead>
+                    <th>No</th>
+                    <th>Judul</th>
+                    <th>Tahun Rilis</th>
+                    <th>Sutradara</th>
+                    <th>Aksi</th>
+                </thead>
+                <tbody>
+                    {
+                        loading && <tr>
+                            <td colSpan={5}>Loading.....</td>
+                        </tr>
+                    }
+                    {
+                        movies.length > 0 && movies.map((movie,index) => {
+                            return <tr key = {movie._id}>
+                                <td>{index + 1}</td>
+                                <td>{movie.judul}</td>
+                                <td>{movie.tahunRilis}</td>
+                                <td>{movie.sutradara}</td>
+                                <td>
+                                    <Button variant="danger" onClick = {() => handleDelete(movie._id)}>Delete</Button>
+                                </td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </Table>
         </div>
     </div>
 }
